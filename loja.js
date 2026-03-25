b11b3b92f604bf54586ac4aacd8bb4036df8250f
NicolasANo <nicolas.nowaski@escola.pr.gov.br>
2026-03-25 11:36:32 +0000

    sla

--- loja.js
+++ loja.js
@@ -1,8 +1,16 @@
 class produto{
-
+   constructor(nome,preco){
+   this.nome = nome
+   this.preco = preco
+   }
 }
 class cliente{
-
+   constructor(){
+    this.produto = []
+   }
+   adicionarProduto(produto){
+     this.produtos.push(produto)
+   }
 }
 class carrinho{
 
